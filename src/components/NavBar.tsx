9abff3227d4d893256fdb4f610c187d64901ecab
Connor <connor27mote@gmail.com>
2022-05-17 15:36:02 -0600

    Update cache after mutations

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,46 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+import { userInfo } from "os";
+import { link } from "fs";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={"auto"}>
-      {/* NextLink uses client side routing */}
-        <NextLink href={"/login"}> uses 
+  const [{ data, fetching }] = useMeQuery();
+  let body;
+
+  //loading
+  if (fetching) {
+    body = null;
+    //user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href={"/login"}>
           <Link mr={2}>login</Link>
         </NextLink>
         <NextLink href={"/register"}>
           <Link>register</Link>
         </NextLink>
+      </>
+    );
+    //user is logged in
+  } else if (data.me.username) {
+    body = (
+      <Flex>
+        {" "}
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tan" p={4}>
+      <Box ml={"auto"}>
+        {/* NextLink uses client side routing */}
+        {body}
       </Box>
     </Flex>
   );
